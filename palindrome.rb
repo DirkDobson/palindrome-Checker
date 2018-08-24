6643edeaf241402e473c6ddca8c2fa258e20cdcf
Dirk Dobson <dirkvancouver@live.com>
2018-08-24 10:36:33 -0600

    inital commit

---
+++ palindrome.rb
@@ -0,0 +1,15 @@
+
+def palin 
+  word = gets.to_s
+  word.split('')
+  puts word
+  word.each do |letter|
+    for i = 0 i < word.length ++i
+    
+    if word[i] = letter
+     puts words
+  end
+end
+end
+
+palin
